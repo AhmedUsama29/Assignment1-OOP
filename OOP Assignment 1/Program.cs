0db675098ef61bb7a1fd9e4086a2d53f793e2121
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-01 20:57:44 +0200

    Q2

--- OOP Assignment 1/Program.cs
+++ OOP Assignment 1/Program.cs
@@ -3,7 +3,7 @@
 namespace OOP_Assignment_1
 {
 
-    #region Q1
+    #region Q1 Enum
 
     public enum WeekDays
     {
@@ -18,22 +18,46 @@ namespace OOP_Assignment_1
 
     #endregion
 
+    #region Q2 struct
+
+    public struct Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
         {
             #region Q1: Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
 
-            Console.WriteLine("Days of the week are:");
 
-            foreach (WeekDays day  in Enum.GetValues(typeof(WeekDays)))
-            {
-                Console.WriteLine(day);
-            }
+            //foreach (WeekDays day  in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(day);
+            //}
 
             #endregion
 
-            #region Q2: 
+            #region Q2: Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            //Person[] people = new Person[3];
+            //people[0] = new Person { Name = "Ahmed", Age = 20 };
+            //people[1] = new Person { Name = "Kareem", Age = 15 };
+            //people[2] = new Person { Name = "Omar", Age = 25 };
+
+            //Console.WriteLine("People details:");
+            //foreach (Person person in people)
+            //{
+            //    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            //}
+
+            #endregion
+
+            #region Q3: 
 
             #endregion
 
