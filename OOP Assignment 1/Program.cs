99ae1ad8047f670014e2d5d05c4a358d19674104
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-01 20:51:09 +0200

    Q1

--- OOP Assignment 1/Program.cs
+++ OOP Assignment 1/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace OOP_Assignment_1
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OOP_Assignment_1
 {
+
+    #region Q1
+
+    public enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1: Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+
+            Console.WriteLine("Days of the week are:");
+
+            foreach (WeekDays day  in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+
+            #endregion
+
+            #region Q2: 
+
+            #endregion
+
         }
     }
 }
