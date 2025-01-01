43b3f6b6c80f69bc865ffa673dcd69be39c49dc4
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-01 21:31:01 +0200

    Q4

--- OOP Assignment 1/Program.cs
+++ OOP Assignment 1/Program.cs
@@ -41,6 +41,19 @@ namespace OOP_Assignment_1
 
     #endregion
 
+    #region Q4 enum
+
+    [Flags]
+    public enum Permissions
+    {
+        Read = 1,
+        Write = 2,
+        Delete = 4,
+        Execute = 8
+    }
+
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
@@ -72,38 +85,66 @@ namespace OOP_Assignment_1
 
             #region Q3: Create an enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
 
-            Console.WriteLine("Write the Season name:");
-            string season = Console.ReadLine();
+            //Console.WriteLine("Write the Season name:");
+            //string season = Console.ReadLine();
 
-            bool flag = Enum.TryParse(season, true, out Season SeasonEnum); //true bec not case sensitive
+            //bool flag = Enum.TryParse(season, true, out Season SeasonEnum); //true bec not case sensitive
 
-            if (flag) {
+            //if (flag) {
 
-                switch (SeasonEnum) {
+            //    switch (SeasonEnum) {
 
-                    case Season.Spring:
-                        Console.WriteLine("Spring: March to May");
-                        break;
-                    case Season.Summer:
-                        Console.WriteLine("Summer: June to August");
-                        break;
-                    case Season.Autumn:
-                        Console.WriteLine("Autumn: September to November");
-                        break;
-                    case Season.Winter:
-                        Console.WriteLine("Winter: December to February");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Season");
-                        break;
+            //        case Season.Spring:
+            //            Console.WriteLine("Spring: March to May");
+            //            break;
+            //        case Season.Summer:
+            //            Console.WriteLine("Summer: June to August");
+            //            break;
+            //        case Season.Autumn:
+            //            Console.WriteLine("Autumn: September to November");
+            //            break;
+            //        case Season.Winter:
+            //            Console.WriteLine("Winter: December to February");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid Season");
+            //            break;
 
-                }
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Season");
+            //}
+
+
+            #endregion
+
+            #region Q4: Assign the following Permissions (Read, write, Delete, Execute) in a form of Enum. Create Variable from previous Enum to Add and Remove Permission from variable, check if specific Permission is existed inside variable
+
+            Permissions permissions = (Permissions)10;
+
+            //current permissions
+            Console.WriteLine("Current Permissions: " + permissions);
+
+            // Remove a permission
+            permissions ^= Permissions.Write;
+
+            // Add a permission
+            permissions ^= Permissions.Read;
+
+            // Display the permissions after
+            Console.WriteLine("Permissions after: " + permissions);
+
+            //check
+            if ((permissions & Permissions.Read) == Permissions.Read)
+            {
+                Console.WriteLine("Read permission exists");
             }
             else
             {
-                Console.WriteLine("Invalid Season");
+                Console.WriteLine("Read permission does not exist");
             }
-
 
             #endregion
 
